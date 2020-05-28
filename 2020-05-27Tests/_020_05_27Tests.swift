23c43e954052edff2ac2766138976fe4d5ffde86
Sam Foulger <samfoulger@gmail.com>
2020-05-27 17:56:35 -0700

    Initial Commit

---
+++ 2020-05-27Tests/_020_05_27Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _020_05_27Tests.swift
+//  2020-05-27Tests
+//
+//  Created by selfoulger on 5/27/20.
+//  Copyright © 2020 Sam Foulger. All rights reserved.
+//
+
+import XCTest
+@testable import _020_05_27
+
+class _020_05_27Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
